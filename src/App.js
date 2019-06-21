b46625520c07a239d8f9009c16be99b9f9862014
marta <martadipuccio@me.com>
2019-06-21 16:35:28 +0200

    range sliders components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component, Fragment} from 'react';
 import MoviesList from './components/MoviesList';
 import Header from './components/Header';
+import YearRangeSlider from './components/YearRangeSlider';
+import RatingRangeSlider from './components/RatingRangeSlider';
+import RuntimeRangeSlider from './components/RuntimeRangeSlider';
+import { Col } from 'react-bootstrap';
+
 class App extends Component {
 
 
   render(){
     return (
-      <div className="App">
-        <Header />
-        <MoviesList />
+      <Fragment>
+      <Header />
+      <div className="container">
+      {/* <Col xs={12} sm={12} md={6} lg={6}>  */}
+
+        <YearRangeSlider className="rangeSlider"/>
+        <RatingRangeSlider />
+        <RuntimeRangeSlider />
+      <div className="gridElement">
+
+      <MoviesList className="movieContainer" />
+</div>
 
       </div>
+      </Fragment>
     );
   }
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import { Col, Row } from 'react-bootstrap';
 
 import { withStyles } from '@material-ui/core/styles';
 //import { getMovieDB } from './utils/api';
@@ -35,12 +35,10 @@ class MovieCard extends Component{
     console.log(this.props);
 
     return(
-     <Grid container className={this.props.classes.root} spacing={2}>
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={2}>
+     <div>
           {[0, 1, 2].map(value => (
-            <Grid key={value} item>
-              <Card className={this.props.classes.card}>
+
+              <Card className='cardWrapper'>
                 <CardMedia
                   title='hello'
 
@@ -60,12 +58,9 @@ class MovieCard extends Component{
                   </div>
                 </CardContent>
                 </Card>
-            </Grid>
-          ))}
-        </Grid>
-        </Grid>
-        </Grid>
 
+          ))}
+          </div>
     )
   }
 
